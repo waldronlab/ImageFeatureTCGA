44ca645bbfc0c9f5295fbc7896b104f19a72c502
LiNk-NY <marcel.ramos@sph.cuny.edu>
2026-02-04 17:05:14 -0500

    pass bfc to .cache_url_file in .download_catalog

--- R/listFiles.R
+++ R/listFiles.R
@@ -105,8 +105,12 @@ getCatalog <-
         req_perform() |>
         resp_body_json()
 
+    cache <- getOption(
+        "BiocFileCache.cache", BiocFileCache::getBFCOption("CACHE")
+    )
+    bfc <- BiocFileCache::BiocFileCache(cache = cache)
     .cache_url_file(
-        resp$files[[1L]]$links$self, redownload = redownload
+        resp$files[[1L]]$links$self, redownload = redownload, bfc = bfc
     )
 }
 
